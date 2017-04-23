2c00e1b094c832299917d5f3698b220251dcb6e8
Avogardo <avogardo0@gmail.com>
2017-04-23 21:06:13 +0200

    Remove autopublish, aff subscription handler

--- imports/components/Main/index.js
+++ imports/components/Main/index.js
@@ -1,17 +1,29 @@
 import { Meteor } from 'meteor/meteor';
 import { FullPageLoader } from '/imports/components/Loaders';
-import { TableData, actions as tableActions } from '/imports/api/tableData';
 import { compose } from 'react-komposer';
+
+import {
+  TableData,
+  actions as tableActions
+} from '/imports/api/tableData';
 
 import App from './App.jsx';
 
 
-const composer = ({}, onData) => {
+const composer = (props, onData) => {
   const projectHandler = Meteor.subscribe('rows');
+console.log(projectHandler);
 
-  onData(null, {
-    addNewRow: tableActions.addNewRow,
-  });
+  if (projectHandler.ready()) {
+    console.log('ready');
+
+    const rows = TableData.find({}).fetch();
+    console.log(rows);
+
+    onData(null, {
+      addNewRow: tableActions.addNewRow,
+    });
+  }
 };
 
 export default compose(
--- server/main.js
+++ server/main.js
@@ -2,4 +2,5 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   import '/imports/api/tableData';
+  import '/imports/api/tableData/server';
 });
