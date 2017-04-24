dfb3fbd4de2ff378bb47755ea1d0a2b582379b66
Avogardo <avogardo0@gmail.com>
2017-04-24 20:37:56 +0200

    add updateThisRow action

--- imports/api/tableData/actions.js
+++ imports/api/tableData/actions.js
@@ -1,6 +1,7 @@
 import {
   addRow,
   removeRow,
+  updateRow,
 } from './methods.js';
 
 const addNewRow = (name, surname, dateFrom, dateTo, callback = () => {}) =>
@@ -9,9 +10,13 @@ const addNewRow = (name, surname, dateFrom, dateTo, callback = () => {}) =>
 const deleteRow = (id, callback = () => {}) =>
   removeRow.call({ id }, callback);
 
+const updateThisRow = (id, name, surname, dateFrom, dateTo, callback = () => {}) =>
+  updateRow.call({ id, name, surname, dateFrom, dateTo }, callback);
+
 const actions = {
   addNewRow,
   deleteRow,
+  updateThisRow,
 };
 
 
